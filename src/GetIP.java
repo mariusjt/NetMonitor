28914c9cc346a4c0de7deffbc98ca9a705ca9c4f
Lindsethen <henrik.lindseth1@gmail.com>
2019-05-29 16:26:30 +0200

    Update GetIP.java
    Updated IPv4 and IPv6

--- src/GetIP.java
+++ src/GetIP.java
@@ -1,9 +1,11 @@
-import java.net.InetAddress;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
 import java.net.UnknownHostException;
 
 public class GetIP {
 
     public static void main(String[] args) throws UnknownHostException {
-        System.out.println(InetAddress.getLocalHost());
+        System.out.println(Inet4Address.getLocalHost());
+        System.out.println(Inet6Address.getLocalHost());
     }
 }
