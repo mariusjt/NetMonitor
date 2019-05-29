ff693bd90ee7a20d66eda8c6b5b887b374034db1
Jostein Rygg <joster17@uia.no>
2019-05-29 13:21:07 +0200

    Made gUI work

--- src/GUI.java
+++ src/GUI.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class GUI extends JFrame {
     private JLabel item1;
-
+    private JLabel item2;
     public GUI()
     {
         super("NETWORK TESTER");
@@ -14,11 +14,6 @@ public class GUI extends JFrame {
         item1 = new JLabel("this is a sentence");
         item1.setToolTipText("Your ping is: ");
         add(item1);
-
-        GUI bucky = new GUI();
-        bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        bucky.setSize(275, 180);
-        bucky.setVisible(true);
 
     }
 
