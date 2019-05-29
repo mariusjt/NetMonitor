25a85359b3de3cc5bb19dedfe52ea2dee277a3bd
Marius T <matjot@gmail.com>
2019-05-29 16:39:47 +0200

    Fixing buttons and ping

--- src/GUI.java
+++ src/GUI.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
 
 public class GUI extends JFrame {
     private JLabel item1;
     private JLabel pingviewer;
     private JLabel speedtest;
-    int ping;
+    Button b;
+    int ping = 100;
     public GUI() {
         super("NETWORK TESTER");
         setLayout(new FlowLayout());
@@ -20,31 +20,34 @@ public class GUI extends JFrame {
             e.printStackTrace();
         }
         long speed = client.getSpeed(); */
-        PingIP pinger = new PingIP();
-        item1 = new JLabel("Hover over this text to show ping");
-        item1.setToolTipText("Your ping is: " + pinger.ping());
-        add(item1);
-        pingviewer = new JLabel("PING: " + ping + "ms" );
-        pingviewer.setVerticalAlignment(0);
+        //PingIP pinger = new PingIP();
+        //item1 = new JLabel("Hover over this text to show ping");
+        //item1.setToolTipText("Your ping is: " + pinger.ping());
+        //add(item1);
+        //pingviewer = new JLabel("PING: " + ping + "ms" );
+        //pingviewer.setVerticalAlignment(0);
         //add(pingviewer);
         //speedtest = new JLabel("Speed: " + speed + "KB/s");
        // add(speedtest);
-        final JTextField tf=new JTextField();
-        tf.setBounds(150,50,150,30);
-        JButton b=new JButton("Click Here");
-        b.setBounds(50,100,95,30);
+        //final JTextField tf=new JTextField();
+        TextField pingbox = new TextField(20);
+        pingbox.setText("not checked");
+        add(pingbox);
+        //tf.setBounds(150,50,150,30);
+        b = new Button("Click Here");
+        //b.setBounds(50,100,95,30);
         b.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 ping();
+                pingbox.setText("Ping: " + ping + "ms");
             }
         });
         add(b);
-        add(tf);
+        //add(tf);
 
     }
     private void ping(){
         PingIP pinger = new PingIP();
         ping = pinger.ping();
-        add(pingviewer);
     }
 }
