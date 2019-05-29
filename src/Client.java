d3e8e716534da596c76610a726515209e9a1e873
Marius T <matjot@gmail.com>
2019-05-29 13:05:46 +0200

    Abstraction of client

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,20 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Client {
-Socket socket;
-File file;
-InputStream in;
-OutputStream out;
-int count;
+
+    private Socket socket;
+    private File file;
+    private InputStream in;
+    private OutputStream out;
+    private int count;
+    long speed;
+    private boolean tested = false;
+    ArrayList speedList = new ArrayList(long);
+
     public Client(int fileSize) throws IOException {
+        System.out.println("Starting file transfer client!");
         socket = new Socket("127.0.0.1", 3333);
         FileGen fg = new FileGen();
         file = fg.getFile(fileSize);
@@ -15,40 +22,28 @@ int count;
         in = new FileInputStream(file);
         out = socket.getOutputStream();
         count = 0;
+        long start = System.currentTimeMillis();
         while (true) {
 
                 if (!((count = in.read(bytes)) > 0)) break;
 
                 out.write(bytes, 0, count);
         }
-
+        long end = System.currentTimeMillis();
+        final long BYTES_IN_KB = 1024;
+        long transferred = BYTES_IN_KB * fileSize;
+        System.out.println("File transfered!");
+        speed = transferred/(end-start);
+        System.out.println("Speed: " + speed + "KB/s");
+        tested = true;
         out.close();
         in.close();
         socket.close();
+    }
 
-
-    /*
-    public static void main(String[] args) throws IOException {
-        Socket socket = null;
-        String host = "127.0.0.1";
-
-        socket = new Socket(host, 5555);
-
-        File file = new File("C:\\Dev\\Test\\1.xml");
-        // Get the size of the file
-        long length = file.length();
-        byte[] bytes = new byte[16 * 1024];
-        InputStream in = new FileInputStream(file);
-        OutputStream out = socket.getOutputStream();
-
-        int count;
-        while ((count = in.read(bytes)) > 0) {
-            out.write(bytes, 0, count);
-        }
-
-        out.close();
-        in.close();
-        socket.close();
-    } */
+    public long getSpeed(){
+        if(tested) {
+            return speed;
+        } else return -1;
     }
 }
