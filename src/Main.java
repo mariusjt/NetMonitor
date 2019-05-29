3c38a8bab17f93d3bdd359d26cc8d87be49d9013
Marius T <matjot@gmail.com>
2019-05-29 14:22:53 +0200

    Added some fixes to GUI and startserv class

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-  /*      System.out.println("Starting server!");
-        Server rx = new Server(3333);
-*/
+  /*     System.out.println("Starting server!");
+        Server rx = new Server(3333); */
+
         GUI gui = new GUI();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gui.setSize(480, 360);
