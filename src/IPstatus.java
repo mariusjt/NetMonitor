b9ed0758b8ec080f06d3294b166957fc4a1c99cc
Joakim Schjølberg <joakimsch@hotmail.com>
2019-05-29 13:14:29 +0200

    Create IPstatus.java

---
+++ src/IPstatus.java
@@ -0,0 +1,24 @@
+package com.mkyong;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IPstatus {
+
+    public static void main(String[] args) {
+
+        InetAddress ip;
+        try {
+
+            ip = InetAddress.getLocalHost();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
