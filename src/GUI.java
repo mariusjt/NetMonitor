d5b0f83c0d9c821f1ab6ec571fa656b07454508c
Jostein Rygg <joster17@uia.no>
2019-05-29 14:04:22 +0200

    fixed sentence

--- src/GUI.java
+++ src/GUI.java
@@ -10,7 +10,7 @@ public class GUI extends JFrame {
         setLayout(new FlowLayout());
 
         PingIP pinger = new PingIP();
-        item1 = new JLabel("this is a sentence");
+        item1 = new JLabel("Hover to get ping");
         item1.setToolTipText("Your ping is: " + pinger.ping());
         add(item1);
     }
