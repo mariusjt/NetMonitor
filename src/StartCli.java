bfdd45f3d5f00dcef0c3bc4bddafe6d8595f7529
Marius T <matjot@gmail.com>
2019-05-29 13:33:35 +0200

    Split client into multiple methods

--- src/StartCli.java
+++ src/StartCli.java
@@ -2,7 +2,13 @@ import java.io.IOException;
 
 public class StartCli {
     public static void main(String[] args) throws IOException {
-        Client tx = new Client(1, "127.0.0.1", 3333);
-        System.out.println("Client started!");
+        Client tx = new Client("127.0.0.1", 3333);
+        System.out.println("Starting client!");
+        tx.speedTest(10);
+        long speed1 = tx.getSpeed();
+        tx.speedTest(2);
+        long speed2 = tx.getSpeed();
+        System.out.println("test1: " + speed1 + "test2: " + speed2);
+        tx.close();
     }
 }
