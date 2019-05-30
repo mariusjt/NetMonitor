7f6d1ff5426f81b2b3fc4b332920a6182a666b30
Lindsethen <henrik.lindseth1@gmail.com>
2019-05-30 12:07:00 +0200

    Update StartCli.java
    Added comments to StartCli

--- src/StartCli.java
+++ src/StartCli.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 
+/* A launcher for Java class Client. */
 public class StartCli {
     public static void main(String[] args) throws IOException {
         Client tx = new Client("127.0.0.1", 3333);
@@ -8,6 +9,7 @@ public class StartCli {
         long speed1 = tx.getSpeed();
         tx.speedTest(2);
         long speed2 = tx.getSpeed();
+        /*Return speed test*/
         System.out.println("test1: " + speed1 + "test2: " + speed2);
         tx.close();
     }
